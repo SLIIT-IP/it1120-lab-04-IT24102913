672ea44f1e95374b269671b82a49054e51a41722
IT24102913 <IT24102913@my.sliit.lk>
2024-08-19 22:48:33 +0530

    Update IT24102913Lab4Q1

--- Q1/IT24100353Lab3Q1A.java
+++ Q1/IT24100353Lab3Q1A.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
-
-	public class IT24100353Lab3Q1A
+public class IT24102913Lab4Q1
+{
+          public static void main(String[]args)
 	{
-		public static void main(String[]args)
-		{
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter the price of 1kg of rice:");
-		double pricePerKg = scanner.nextDouble();
-		System.out.print("Enter the number of kilograms you want to buy:");
-		double kilograms = scanner.nextDouble();
-		double totalAmount = pricePerKg * kilograms;
-		System.out.println("The total amount is:"+totalAmount);
-		}
+	         int number;
+			 Scanner input = new Scanner(System.in );
+			 System.out.print("enter a number: ");
+			 number = input.nextInt();
+			 if(number > 0)
+			 {
+			   System.out.print("The number is: Positive");
+			 }
+			 else if (number < 0)
+			 {
+			   System.out.print("The number is: Negative");
+			 }
+			 else if (number == 0)
+			 {
+			   System.out.print("The number is zero");
+			 }
 	}
-		
-		
+}
