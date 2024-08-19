271df5a11742826420dfab653803a0107495d1aa
IT24102913 <IT24102913@my.sliit.lk>
2024-08-19 22:55:50 +0530

    Add files via upload

---
+++ Q3/IT24102913Lab4Q3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT24102913Lab4Q3
+{
+          public static void main(String[]args)
+	{
+	         int number;
+			 String message;
+			 Scanner input = new Scanner(System.in );
+			 System.out.print("enter a number: ");
+			 number = input.nextInt();
+			 message = (number > 0) ? "The number is: Positive" : (number < 0) ? "The number is: Negative" : "The number is zero"; 
+			 System.out.print(message);
+			 
+	}
+}
+			 
+	
